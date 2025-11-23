109ba766a550e52a9fa078c498e7331026823b3e
Mikhail Petrachenka <77238687+aCrYoZPS@users.noreply.github.com>
2025-11-23 21:31:47 +0300

    chart shenanigans

--- code/generate_charts.py
+++ code/generate_charts.py
@@ -9,7 +9,9 @@ def main():
     file_name = sys.argv[1]
     cpu1_data: pd.DataFrame = pd.read_csv(sys.argv[2], sep=";")
     cpu2_data: pd.DataFrame = pd.read_csv(sys.argv[3], sep=";")
-    data_type = f"{sys.argv[2].split("_")[0]} {sys.argv[2].split(".csv")[0].split("_")[-1]}"
+    cpu1_data = cpu1_data[cpu1_data["Iters"] >= 1000]
+    cpu2_data = cpu2_data[cpu2_data["Iters"] >= 1000]
+    data_type = f"{sys.argv[2].split("_")[0][2:]} {sys.argv[2].split(".csv")[0].split("_")[-1]}"
     num_iters = cpu1_data["Iters"].unique()
 
     cpu1_data["Times"] = cpu1_data["Times"] / 1000
@@ -18,8 +20,8 @@ def main():
     average_times_cpu1 = cpu1_data.groupby("Iters")["Times"].mean()
     average_times_cpu2 = cpu2_data.groupby("Iters")["Times"].mean()
 
-    # fig, ax = plt.subplots(figsize=(8, 10))
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=(10, 8))
+    # fig, ax = plt.subplots()
     ax.plot(num_iters, average_times_cpu1, marker='o', linestyle='-', label='Ryzen 5 3500U')
     ax.plot(num_iters, average_times_cpu2, marker='s', linestyle='--', label='Ryzen 7 7700')
 
@@ -30,8 +32,8 @@ def main():
     # ax.set_yscale('log')
 
     ax.set_xticks(num_iters)
-    ax.set_yticks(list(average_times_cpu1) + list(average_times_cpu2))
-    ax.tick_params(axis='y', labelrotation=45)
+    # ax.set_yticks(list(average_times_cpu1) + list(average_times_cpu2))
+    ax.tick_params(axis='x', labelrotation=45)
 
     ax.xaxis.set_major_formatter(ticker.ScalarFormatter())
     ax.yaxis.set_major_formatter(ticker.ScalarFormatter())
